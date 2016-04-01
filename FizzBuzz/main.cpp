b32b4f19bdcb3f852272c97d39f029d9493e39b1
Andrew Burnie <apburnie@hotmail.co.uk>
2016-04-01 18:49:52 +0100

    tests only on demand

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -16,33 +16,47 @@ int main() {
     int integer;
     FizzBuzz game;
     Tests test;
+    string response;
     
-    cout << "Below are the test results: " << endl << endl
-    << "1. Fizzes? " << test.test(3) << endl
-    << "2. Buzzes? " << test.test(5) << endl
-    << "3. FizzBuzzes? " << test.test(15) << endl
-    << "4. Otherwise returns integer? " << test.test(7) << endl
-    << "5. Rejection of negative numbers? " << test.test(-7) << endl
-    << "6. Nothing returned with 0? " << test.test(0) << endl << endl;
+    cout << "Display test results?" << endl;
+    cin >> response;
+    cout << endl;
     
-    cout << "Program Guidance: " << endl << endl;
+    if (response[0] == 'Y' || response[0] == 'y')
+    {
+        cout << "Below are the test results: " << endl << endl
+             << "1. Fizzes? " << test.test(3) << endl
+             << "2. Buzzes? " << test.test(5) << endl
+             << "3. FizzBuzzes? " << test.test(15) << endl
+             << "4. Otherwise returns integer? " << test.test(7) << endl
+             << "5. Rejection of negative numbers? " << test.test(-7) << endl
+             << "6. Nothing returned with 0? " << test.test(0) << endl << endl;
+    }
     
-    cout << "1. Must give a number not text" << endl
-         << "2. If given number is not an integer, rounded down to the nearest integer" << endl
-         << "3. Typing 0 will terminate the program" << endl << endl;
+    cout << "Continue?" << endl;
+    cin >> response;
+    cout << endl;
     
-    cout << "Below is the program: " << endl << endl;
+    if (response[0] == 'Y' || response[0] == 'y')
+    {
+        cout << "Program Guidance: " << endl << endl
+             << "1. Must give a number not text" << endl
+             << "2. Decimals rounded down to the nearest integer" << endl
+             << "3. Negative numbers not permitted" << endl
+             << "4. Typing 0 will terminate the program" << endl << endl;
+        
+        cout << "Below is the program: " << endl << endl;
     
     do
     {
-    
-    cout << "Please give an integer: ";
-    cin >> integer;
-    
-    cout << game.play(integer) << endl;
+        cout << "Please give an integer: ";
+        cin >> integer;
+        cout << game.play(integer) << endl;
     }
     
     while (integer != 0);
+    
+    }
     
     return 0;
 }
